0af8120a1991a11f55c4e4535c5f3fef93e65a44
DaveBit <dewidhoct@gmail.com>
2022-10-01 16:38:33 +0200

    chapter 4 exercises 01/10/2022

---
+++ chapter4/buffet.py
@@ -0,0 +1,15 @@
+basicfoods = ('Paella', 'sausage',
+              'falafel', 'halusky',
+              'rice in the oven')
+print('You can choose from the folowwing menu tiems: ')
+for food in basicfoods:
+    print('- ', food)
+# basicfoods[0] = 'parrot'  tuples do not accept item assignments
+
+print('\nOur menu has been updated')
+print('\nYou can choose from the folowwing menu tiems: ')
+basicfoods = ('Squid', 'Puchero',
+              'falafel', 'halusky',
+              'rice in the oven')
+for food in basicfoods:
+    print('- ', food)
