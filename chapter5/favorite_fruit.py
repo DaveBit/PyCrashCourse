c6d01752b2f93555377c577523e8a26d321bca10
DaveBit <dewidhoct@gmail.com>
2022-10-04 20:41:39 +0200

    chapter 5 exercises 04/10/2022

---
+++ chapter5/favorite_fruit.py
@@ -0,0 +1,14 @@
+favorite_fruits = ['orange', 'tomato', 'pear', 'banana']
+
+if 'orange' in favorite_fruits:
+    print('I love oranges')
+if 'tomato' in favorite_fruits:
+    print('I like tomatoes very much')
+if 'pear' in favorite_fruits:
+    print('I love pears')
+if 'banana' in favorite_fruits:
+    print("I don''t like bananas")
+if 'kiwi' in favorite_fruits:
+    print('I love kiwis')
+
+
