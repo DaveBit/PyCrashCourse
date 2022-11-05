d2a25d8752827f8af68d41193c0d6547838860b9
DaveBit <dewidhoct@gmail.com>
2022-11-05 01:09:48 +0100

    chapter 8 exercises 05/11/2022

---
+++ chapter8/pizza.py
@@ -0,0 +1,7 @@
+def make_pizza(*toppings):
+    """Print the list of toppings that have been requested. """
+    print(toppings)
+
+
+make_pizza('pineapple')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
