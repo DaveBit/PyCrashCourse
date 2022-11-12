034b2e4d9e4aed7948ff733a528b229117cace60
DaveBit <dewidhoct@gmail.com>
2022-11-12 22:29:52 +0100

    chapter 9 12/11/2022

---
+++ chapter9/privileges_tiy.py
@@ -0,0 +1,84 @@
+class User:
+    def __init__(self, first_name, last_name, username, email, location):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        self.location = location
+        self.login_attempts = 0
+
+    def describe_user(self):
+        """Display a summary of the user's information."""
+        print("\n" + self.first_name + " " + self.last_name)
+        print("  Username: " + self.username)
+        print("  Email: " + self.email)
+        print("  Location: " + self.location)
+
+    def greet_user(self):
+        """Display a personalized greeting to the user."""
+        print("\nWelcome back, " + self.username + "!")
+
+    def increment_login_attempts(self):
+        """Increment the value of login_attempts."""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reset login_attempts to 0."""
+        self.login_attempts = 0
+
+
+class Admin(User):
+    """A user with administrative privileges."""
+
+    def __init__(self, first_name, last_name, username, email, location):
+        """Initialize the admin."""
+        super().__init__(first_name, last_name, username, email, location)
+
+        # Initialize an empty set of privileges.
+        self.privileges = Privileges()
+
+    def describe_user(self):
+        """Display a summary of the user's information."""
+        print("\n" + self.first_name + " " + self.last_name)
+        print("  Username: " + self.username)
+        print("  Email: " + self.email)
+        print("  Location: " + self.location)
+        print("  User type: Admin")
+
+
+class Privileges:
+    """A class to store an admin's privileges."""
+
+    def __init__(self, privileges=[]):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print("\nPrivileges:")
+        if self.privileges:
+            for privilege in self.privileges:
+                print("- " + privilege)
+        else:
+            print("- This user has no privileges.")
+
+    def add_privileges(self, *privileges):
+        for privilege in privileges:
+            self.privileges.append(privilege)
+
+
+admin1 = Admin('Dave', 'Bit', 'Davebit', 'davebitdestroyer', 'london')
+admin1.privileges.show_privileges()
+#  admin1.privileges.privileges.append()
+admin1.privileges.add_privileges('add a user', 'delete a user', 'ban a user')
+admin1.privileges.show_privileges()
+admin1.describe_user()
+
+"""
+Also:
+admin1_privileges = [
+    'can reset passwords',
+    'can moderate discussions',
+    'can suspend accounts',
+    ]
+admin1.privileges.privileges = eric_privileges
+admin1.privileges.show_privileges()
+"""
