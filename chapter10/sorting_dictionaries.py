626fe4934ece01e2305529c8af9e6397fafb0b07
DaveBit <dewidhoct@gmail.com>
2022-11-18 14:49:38 +0100

    chapter 10 18/11/2022

--- chapter10/sorting_dictionaries.py
+++ chapter10/sorting_dictionaries.py
@@ -4,8 +4,9 @@ def count_words(filename):
         with open(filename) as f_obj:
             contents = f_obj.read()
     except FileNotFoundError:
-        msg = "Sorry, the file " + filename + " does not exist."
-        print(msg)
+        #  msg = "Sorry, the file " + filename + " does not exist."
+        # print(msg)
+        pass
     else:
         #  Count the approximate number of words in the file.
         words = contents.split()
@@ -33,12 +34,9 @@ print("The longest book is " + (list(books_dictionary_sorted.keys())[0]).replace
 print("The longest book is " + next(iter(books_dictionary_sorted)).replace('_', ' ').title())
 #  or
 max_value = max(books_dictionary.values())
-print(max_value)
 print("The longest book is " + str(max(books_dictionary, key=books_dictionary.get)))
 #  max looks for the highest value. First argument is the iterable.
 #  second argument is the key we are going to use to find the max value.
 #  we are using books_dictionary.get since it retrieves the value for each key.
 
-print(max(books_dictionary))
-print(books_dictionary)
 
